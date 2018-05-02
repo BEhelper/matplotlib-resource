62dea201123cfeac806fea3412872bc9fd323d05
Brad <brad.taris@gmail.com>
2018-05-01 22:58:29 -0700

    Update tutorial-code.py

--- tutorial-code.py
+++ tutorial-code.py
@@ -389,8 +389,6 @@ def load_from_internet_ohlc():
 
 
 
-load_from_internet_ohlc()
-
 
 
 
@@ -426,6 +424,66 @@ def load_live_data():
 
 
 
+
+# SUBPLOTS
+
+def sub_plots():
+    import random
+    import matplotlib.pyplot as plt
+    from matplotlib import style
+
+    style.use('fivethirtyeight')
+
+    fig = plt.figure()
+
+    def create_plots():
+        xs = []
+        ys = []
+
+        for i in range(10):
+            x = i
+            y = random.randrange(10)
+
+            xs.append(x)
+            ys.append(y)
+        return xs, ys
+
+    
+    # 1
+    ax1 = fig.add_subplot(221)
+    x, y = create_plots()
+    ax1.plot(x, y)
+    # 2
+    ax2 = fig.add_subplot(222)
+    x, y = create_plots()
+    ax2.plot(x, y)
+    # 3
+    ax3 = fig.add_subplot(212)
+    x, y = create_plots()
+    ax3.plot(x, y)
+
+    """
+        |---------|--------|
+        |   221   |  222   |
+        |---------|--------|
+        |        212       |
+        |------------------|
+
+        OR
+
+        |---------|--------|
+        |   221   |  222   |
+        |---------|--------|
+        |   223   |  224   |
+        |---------|--------|
+
+    """
+
+
+    plt.show()
+
+
+sub_plots()
 
 
 
